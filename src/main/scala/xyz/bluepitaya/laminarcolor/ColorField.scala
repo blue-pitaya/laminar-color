f67a954c6f90c307d3d6129a81c23d959b3fd054
blue-pitaya <blue.pitaya@pm.me>
2023-01-03 17:11:03 +0100

    added color field component

---
+++ src/main/scala/xyz/bluepitaya/laminarcolor/ColorField.scala
@@ -0,0 +1,36 @@
+package xyz.bluepitaya.laminarcolor
+
+import com.raquo.laminar.api.L._
+import org.scalajs.dom
+
+object TransparentImage {
+  def data =
+    "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAQ5JREFUOE9tU9sRwzAIEzt4/wm9Az2QeCRpPmL3ApKQqAF2AMDgccBh+c6H11uX+TQ1ZsCJpri4s5H3Lrp9M4O5P0ji2wlk89Ag5kUPw0WCERgBQIYURoChbd09gYEKWpG6gowYOCUxz361Lzd0LWfInP22FdC3/Yj50lRBpPyqouXnha8E1LBT+KeEMWqYwHOXWcRx4JrJ4PKhxAhQ4h5Tdg6hcGdTBmQui5dj7R1SUhVzyUk1oVKGM2A1l7MFzd0Ij4IqHPeMn+Vkq3NQluPayLNXbKKm4gRI5tGimDps7snqbPmzjONIgOkXNK9WvUBeXuUo3VFUUcRV7Zh3LmV/pfDdvwEwU4zz73yU/wA76oy6jX5IIgAAAABJRU5ErkJggg=="
+}
+
+object ColorField {
+  val darkBorderStyle = Seq(
+    boxShadow(
+      "rgb(0 0 0 / 15%) 0px 0px 0px 1px inset, rgb(0 0 0 / 25%) 0px 0px 4px inset"
+    )
+  )
+
+  val lightBorderStyle =
+    Seq(boxShadow("rgb(0 0 0 / 10%) 0px 0px 0px 1px inset"))
+
+  val circleStyle = Seq(borderRadius("50%"))
+
+  /** Size of component is defined by parent div */
+  def component(
+      color: Var[ColorPicker.Hsv],
+      modifiers: Seq[Setter[HtmlElement]] = Seq()
+  ) = Seq(
+    div(
+      width("100%"),
+      height("100%"),
+      backgroundColor <-- color.signal.map(_.toCssRgb),
+      modifiers,
+      div(background(s"url('${TransparentImage.data}')"))
+    )
+  )
+}
--- src/main/scala/xyz/bluepitaya/laminarcolor/pickers/ChromePicker.scala
+++ src/main/scala/xyz/bluepitaya/laminarcolor/pickers/ChromePicker.scala
@@ -5,15 +5,24 @@ import org.scalajs.dom
 import xyz.bluepitaya.laminarcolor.ColorPicker
 import xyz.bluepitaya.laminarcolor.Saturation
 import xyz.bluepitaya.laminarcolor.SaturationStyles
+import xyz.bluepitaya.laminarcolor.ColorField
 
 object ChromePicker {
   val saturationStyle = Seq(cls("chromePickerSaturationContainer"))
 
   val circleStyle = SaturationStyles.circle(12, 12)
 
-  def component(color: Var[ColorPicker.Hsv]) = div(
-    width("225px"),
-    Saturation.component(color, saturationStyle, circleStyle),
-    div(padding("16px 16px 12px"), backgroundColor("red"))
-  )
+  def component(color: Var[ColorPicker.Hsv]) = {
+    val colorField = ColorField
+      .component(color, ColorField.lightBorderStyle ++ ColorField.circleStyle)
+
+    div(
+      width("225px"),
+      Saturation.component(color, saturationStyle, circleStyle),
+      div(
+        padding("16px 16px 12px"),
+        div(width("16px"), height("16px"), colorField)
+      )
+    )
+  }
 }
