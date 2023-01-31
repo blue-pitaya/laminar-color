7d2765b7fbd4dab62f5988777bc0606c7d469e83
blue-pitaya <blue.pitaya@pm.me>
2023-01-31 23:46:16 +0100

    moved implicit for Var[Hsv] to State

--- example/src/main/scala/xyz/bluepitaya/example/Main.scala
+++ example/src/main/scala/xyz/bluepitaya/example/Main.scala
@@ -2,37 +2,13 @@ package xyz.bluepitaya.example
 
 import com.raquo.laminar.api.L._
 import org.scalajs.dom
-import xyz.bluepitaya.laminarcolor.pickers.ChromePicker
-import xyz.bluepitaya.laminarcolor.pickers.SketchPicker
-import xyz.bluepitaya.laminarcolor.pickers.SimplePicker
-import xyz.bluepitaya.laminarcolor.State
 import xyz.bluepitaya.common.Hsv
+import xyz.bluepitaya.laminarcolor.pickers.ChromePicker
+import xyz.bluepitaya.laminarcolor.pickers.SimplePicker
+import xyz.bluepitaya.laminarcolor.pickers.SketchPicker
 
 object Main extends App {
   val hsvColor = Var(Hsv(200, 0.75, 0.75, 1))
-
-  implicit val hsvVarState = new State[Var[Hsv]] {
-
-    override def signal(a: Var[Hsv]): Signal[Hsv] = a.signal
-
-    override def updateHue(a: Var[Hsv], hue: Double): Unit = a
-      .update(s => s.copy(h = hue))
-
-    override def updateAlpha(a: Var[Hsv], alpha: Double): Unit = a
-      .update(s => s.copy(a = alpha))
-
-    override def updateSaturationAndBrigthness(
-        a: Var[Hsv],
-        s: Double,
-        v: Double
-    ): Unit = a.update(x => x.copy(s = s, v = v))
-
-    override def now(a: Var[Hsv]): Hsv = a.now()
-
-    override def set(a: Var[Hsv], v: Hsv): Unit = a.set(v)
-
-  }
-
   val colorSignal = hsvColor.signal
   val updateHue = (h: Double) => hsvColor.update(c => c.copy(h = h))
 
--- src/main/scala/xyz/bluepitaya/laminarcolor/State.scala
+++ src/main/scala/xyz/bluepitaya/laminarcolor/State.scala
@@ -11,3 +11,26 @@ trait State[A] {
   def now(a: A): Hsv
   def set(a: A, v: Hsv): Unit
 }
+
+object State {
+  implicit val hsvVarState = new State[Var[Hsv]] {
+    override def signal(a: Var[Hsv]): Signal[Hsv] = a.signal
+
+    override def updateHue(a: Var[Hsv], hue: Double): Unit = a
+      .update(s => s.copy(h = hue))
+
+    override def updateAlpha(a: Var[Hsv], alpha: Double): Unit = a
+      .update(s => s.copy(a = alpha))
+
+    override def updateSaturationAndBrigthness(
+        a: Var[Hsv],
+        s: Double,
+        v: Double
+    ): Unit = a.update(x => x.copy(s = s, v = v))
+
+    override def now(a: Var[Hsv]): Hsv = a.now()
+
+    override def set(a: Var[Hsv], v: Hsv): Unit = a.set(v)
+
+  }
+}
