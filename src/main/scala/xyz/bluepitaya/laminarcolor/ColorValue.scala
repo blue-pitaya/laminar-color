44d46891233124ab730e3aab083045a6d06947d5
blue-pitaya <blue-pitaya@users.noreply.github.com>
2023-04-25 00:34:14 +0000

    deploy: b06d3d426494aa65623f2c2bb7d8f9ed0130fe26

--- src/main/scala/xyz/bluepitaya/laminarcolor/ColorValue.scala
+++
@@ -1,9 +0,0 @@
-package xyz.bluepitaya.laminarcolor
-
-import com.raquo.laminar.api.L._
-import xyz.bluepitaya.laminarcolor.models.Hsv
-
-object ColorValue {
-  def rgbComponent(hsv: Var[Hsv]) =
-    div(child.text <-- hsv.signal.map(_.toCssRgb))
-}
--- src/main/scala/xyz/bluepitaya/laminarcolor/models/Rgba.scala
+++
@@ -1,4 +0,0 @@
-package xyz.bluepitaya.laminarcolor.models
-
-/** alpha is 0..1 */
-case class Rgba(r: Int, g: Int, b: Int, a: Double)
